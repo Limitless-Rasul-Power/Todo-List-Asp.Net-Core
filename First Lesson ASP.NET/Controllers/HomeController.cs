94a1eee22d96b26c6296f628d313c60665411652
Osmanli Rasul <r.osmanl@mail.ru>
2022-01-09 01:58:31 +0400

    First Commit

---
+++ First Lesson ASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using First_Lesson_ASP.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_Lesson_ASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static readonly List<Todo> _todos = new();
+        private static int _id;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {            
+            return View(_todos);
+        }
+
+        [HttpPost]
+        public IActionResult AddTodo(Todo task)
+        {
+            task.Id = ++_id;
+            _todos.Add(task);            
+            return RedirectToAction(nameof(HomeController.Index));
+        }
+
+        
+        [HttpGet]
+        public IActionResult DeleteTodo(int id)
+        {
+            _todos.Remove(_todos.Find(t => t.Id == id));
+            return RedirectToAction(nameof(HomeController.Index));
+        }
+
+        [HttpGet]
+        public IActionResult CheckTodo(int id)
+        {
+            Todo todo = _todos.Find(t => t.Id == id);
+            todo.IsDone = !todo.IsDone;
+            return RedirectToAction(nameof(HomeController.Index));
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ First Lesson ASP.NET/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace First_Lesson_ASP.NET.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
